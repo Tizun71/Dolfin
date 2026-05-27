f24b35f1fdaba7c9777ffb951e557c882bc430d4
Riyu <bui85847@gmail.com>
2026-05-27 15:21:10 +0700

    checkpoint: save current state before refactoring

--- frontend/app/(app)/vaults/page.tsx
+++ frontend/app/(app)/vaults/page.tsx
@@ -1,5 +1,4 @@
 import VaultsContent from "@/components/views/vaults/VaultsContent";
-// nextjs khuyển dùng từ server component -> client
 export default function VaultsPage() {
   return <VaultsContent />;
 }
